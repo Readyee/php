d37e75564e2624c51d3a8436a98ef93ccd3c0f92
Readyee <yesthatsmyemail321@gmail.com>
2024-09-30 17:00:38 -0700

    Add files via upload

---
+++ report.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>done</title>
+    </head>
+    <body>
+        <?php
+        $ok = $_POST['bug'];
+        echo 'this will be sent " ' . $ok . ' "'
+        ?>
+    </body>
+</html>
